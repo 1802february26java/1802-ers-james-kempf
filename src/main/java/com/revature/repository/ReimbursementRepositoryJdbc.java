f9c2369020d6607765645fbe70389e0ae29bbd13
James Kempf <jameskempf@Jamess-MacBook-Pro.local>
2018-03-12 15:13:03 -0400

    complete update reimbursement

--- src/main/java/com/revature/repository/ReimbursementRepositoryJdbc.java
+++ src/main/java/com/revature/repository/ReimbursementRepositoryJdbc.java
@@ -58,7 +58,56 @@ public class ReimbursementRepositoryJdbc implements ReimbursementRepository {
 
 	@Override
 	public boolean update(Reimbursement reimbursement) {
-		// TODO Auto-generated method stub
+		logger.trace("Updating reimbursement");
+		try (Connection connection = ConnectionUtil.getConnection()) {
+			int parameterIndex = 0;
+			String sql = "UPDATE REIMBURSEMENT "
+					+ "SET R_ID = ?, "
+					+ "R_REQUESTED = ?, "
+					+ "R_RESOLVED = ?, "
+					+ "R_AMOUNT = ?, "
+					+ "R_DESCRIPTION = ?, "
+					+ "EMPLOYEE_ID = ?, "
+					+ "MANAGER_ID = ?, "
+					+ "RS_ID = ?, "
+					+ "RT_ID = ?"; 
+			
+//			Name          Null?    Type           
+//					------------- -------- -------------- 
+//					R_ID          NOT NULL NUMBER         
+//					R_REQUESTED   NOT NULL TIMESTAMP(6)   
+//					R_RESOLVED             TIMESTAMP(6)   
+//					R_AMOUNT      NOT NULL NUMBER(8,2)    
+//					R_DESCRIPTION          VARCHAR2(4000) 
+//					R_RECEIPT              BLOB           
+//					EMPLOYEE_ID   NOT NULL NUMBER         
+//					MANAGER_ID             NUMBER         
+//					RS_ID         NOT NULL NUMBER         
+//					RT_ID         NOT NULL NUMBER         
+			
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setInt(++parameterIndex, reimbursement.getId());
+			statement.setTimestamp(++parameterIndex, Timestamp.valueOf(reimbursement.getRequested()));
+			if (reimbursement.getResolved() != null) {
+				statement.setTimestamp(++parameterIndex, Timestamp.valueOf(reimbursement.getResolved()));
+			} else {
+				statement.setTimestamp(++parameterIndex, null);
+			}
+			statement.setDouble(++parameterIndex, reimbursement.getAmount());
+			statement.setString(++parameterIndex, reimbursement.getDescription());
+			statement.setInt(++parameterIndex, reimbursement.getRequester().getId());
+			if (reimbursement.getApprover() != null) {
+				statement.setInt(++parameterIndex, reimbursement.getApprover().getId());
+			} else {
+				statement.setNull(++parameterIndex, java.sql.Types.INTEGER);
+			}
+			statement.setInt(++parameterIndex, reimbursement.getStatus().getId());
+			statement.setInt(++parameterIndex, reimbursement.getType().getId());
+
+			return (statement.executeUpdate() > 0);
+		} catch (SQLException e) {
+			logger.error("Exception thrown while Updating reimbursement", e);
+		}
 		return false;
 	}
 
@@ -105,7 +154,8 @@ public class ReimbursementRepositoryJdbc implements ReimbursementRepository {
 		ReimbursementStatus rs = new ReimbursementStatus(1,"PENDING");
 		ReimbursementType rt = new ReimbursementType(1,"OTHER");
 		Reimbursement r = new Reimbursement(100,LocalDateTime.now(),null,10,"Sample",e,null,rs,rt);
-		System.out.println(r.toString());
-		logger.trace(repository.insert(r));
+//		logger.trace(repository.insert(r));
+		r.setAmount(100);
+		logger.trace(repository.update(r));
 	}
 }
