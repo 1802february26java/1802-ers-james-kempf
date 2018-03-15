e038a8882a8095797f39589263e4935d56d030e5
James Kempf <jameskempf@Jamess-MacBook-Pro.local>
2018-03-15 09:06:59 -0400

    complete more service methods

--- src/main/java/com/revature/service/EmployeeServiceAlpha.java
+++ src/main/java/com/revature/service/EmployeeServiceAlpha.java
@@ -1,83 +1,108 @@
 package com.revature.service;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
 import com.revature.model.Employee;
+import com.revature.model.EmployeeRole;
 import com.revature.model.EmployeeToken;
 import com.revature.repository.EmployeeRepository;
 import com.revature.repository.EmployeeRepositoryJdbc;
 
 public class EmployeeServiceAlpha implements EmployeeService {
+	
+	private static EmployeeService service = new EmployeeServiceAlpha();
+	private static Logger logger = Logger.getLogger(EmployeeServiceAlpha.class);
+	private EmployeeRepository repository = EmployeeRepositoryJdbc.getInstance();
+	
+	private EmployeeServiceAlpha() {}
+	
+	public static EmployeeService getInstance() {
+		return service;
+	}
 
 	@Override
 	public Employee authenticate(Employee employee) {
-		EmployeeRepository repository = EmployeeRepositoryJdbc.getInstance();
 		return null;
 	}
 
 	@Override
 	public Employee getEmployeeInformation(Employee employee) {
-		// TODO Auto-generated method stub
-		return null;
+		logger.trace("Getting Employee Information");
+		Employee employeeInfo = repository.select(employee.getId());
+		return employeeInfo;
 	}
 
 	@Override
 	public Set<Employee> getAllEmployeesInformation() {
-		EmployeeRepository repository = EmployeeRepositoryJdbc.getInstance();
+		logger.trace("Getting all employee information");
 		Set<Employee> employees = repository.selectAll();
 		return employees;
 	}
 
 	@Override
 	public boolean createEmployee(Employee employee) {
-		EmployeeRepository repository = EmployeeRepositoryJdbc.getInstance();
+		logger.trace("Creating employee");
 		boolean inserted = repository.insert(employee);
 		return inserted;
 	}
 
 	@Override
 	public boolean updateEmployeeInformation(Employee employee) {
-		EmployeeRepository repository = EmployeeRepositoryJdbc.getInstance();
+		logger.trace("Updating employee");
 		boolean updated = repository.update(employee);
 		return updated;
 	}
 
 	@Override
 	public boolean updatePassword(Employee employee) {
-		EmployeeRepository repository = EmployeeRepositoryJdbc.getInstance();
+		logger.trace("Updating password");
 		boolean updated = repository.update(employee);
 		return updated;
 	}
 
 	@Override
 	public boolean isUsernameTaken(Employee employee) {
-		EmployeeRepository repository = EmployeeRepositoryJdbc.getInstance();
+		logger.trace("Checking if username taken");
 		boolean taken = (repository.select(employee.getUsername()) != null);
 		return taken;
 	}
 
 	@Override
 	public boolean createPasswordToken(Employee employee) {
-		EmployeeRepository repository = EmployeeRepositoryJdbc.getInstance();
-		EmployeeToken employeeToken = new EmployeeToken();
+		logger.trace("Creating password token");
+		LocalDateTime now = LocalDateTime.now();
+		String token = Integer.toString((employee.toString() + now.toString()).hashCode());
+		EmployeeToken employeeToken = new EmployeeToken(0, token, LocalDateTime.now(), employee);
 		boolean created = repository.insertEmployeeToken(employeeToken);
-		return false;
+		return created;
 	}
 
 	@Override
 	public boolean deletePasswordToken(EmployeeToken employeeToken) {
-		// TODO Auto-generated method stub
-		return false;
+		logger.trace("Deleteing password token");
+		boolean deleted = repository.deleteEmployeeToken(employeeToken);
+		return deleted;
 	}
 
 	@Override
 	public boolean isTokenExpired(EmployeeToken employeeToken) {
-		// TODO Auto-generated method stub
-		return false;
+		logger.trace("Cheking if token expired");
+		boolean expired = (repository.selectEmployeeToken(employeeToken) == null);
+		return expired;
 	}
 
 	public static void main(String[] args) {
+		EmployeeRole employeeRole = new EmployeeRole(1, "EMPLOYEE");
+		Employee employee = new Employee(100,"James","Kempf","jamesk4321","password1","example@gmail.com",employeeRole);
 		EmployeeService service = new EmployeeServiceAlpha();
+		// Employee functions
+		System.out.println(service.createEmployee(employee));
+		System.out.println(service.getEmployeeInformation(employee));
 		System.out.println(service.getAllEmployeesInformation());
-	}
+		// Token functions
+		
+		}
 }
